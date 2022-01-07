8aac89e29a1c48f8ecdb33c69b3805a3051ed5e6
Gregory Errecart <gerrecart@iutbayonne.univ-pau.fr>
2022-01-07 18:20:18 +0100

    Premiere ébauche de création de données (fonctionnel)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Entreprise;
+use App\Entity\Formation;
+use App\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
@@ -13,31 +15,75 @@ class AppFixtures extends Fixture
         //Création d'un générateur de données faker
         $faker = \Faker\Factory::create('fr_FR');
 
-        $nbElements = 20;
+        //GESTION DU NOMBRE D'ENTITE
+        $nbEntreprises = 20;
+        $nbFormations = 20;
+        $nbStages = 30;
 
-        for ($i=1; $i <= $nbElements; $i++) 
+
+        /* GESTION DES ENTREPRISES */
+
+        //Gestion du tableau
+        $tabEntreprises = array();
+        
+        //Boucle création des entreprises
+        for ($i=1; $i <= $nbEntreprises; $i++) 
         {             
-            $entreprise1 = new Entreprise();
-            $entreprise1->setActivite("truc");
-            $entreprise1->setAdresse("truc");
-            $entreprise1->setNom("truc");
-            $entreprise1->setUrlSite("truc");
-            $manager->persist($entreprise1);
+            $entreprise = new Entreprise();
+
+            $entreprise->setActivite("activité.$i");
+            $entreprise->setAdresse("adresse.$i");
+            $entreprise->setNom("nom.$i");
+            $entreprise->setUrlSite("url.$i");
+
+            array_push($tabEntreprises,$entreprise);
+
+            $manager->persist($entreprise);
+        }
 
 
-            $formation1 = new Formation();
-            $formation1->setNomLong("truc");
-            $formation1->setNomCourt("truc");
-            $manager->persist($formation1);
+        /* GESTION DES FORMATIONS */
 
+        //Gestion du tableau
+        $tabFormations = array();
 
-            $stage1 = new Stage();
-            $entreprise1 = new Entreprise();
-            $stage1->setTitre($faker->sentence(3));
-            $stage1->setDescription($faker->paragraph(2));
-            $stage1->setEmail($faker->freeEmail());
-            $stage1->setEntreprise($entreprise1);
-            $manager->persist($stage1);
+        //Boucle création des formations
+        for ($i=1; $i <= $nbFormations; $i++)
+        {
+            $formation = new Formation();
+            $formation->setNomLong("nomLong.$i");
+            $formation->setNomCourt("nomCourt.$i");
+
+            array_push($tabFormations,$formation);
+
+            $manager->persist($formation);
+        }
+        
+
+        /* GESTION DES STAGES */
+
+        //Gestion du tableau
+        $tabStages = array();
+
+        //Boucle création des stages
+        for ($i=1; $i <= $nbStages; $i++)
+        {
+            $stage = new Stage();
+
+            $stage->setTitre($faker->sentence(3));
+            $stage->setDescription($faker->paragraph(1));
+            $stage->setEmail($faker->freeEmail());
+
+            $stage->setEntreprise($tabEntreprises[$faker->numberBetween(0,count($tabEntreprises)-1)]);
+
+            $nbFormationStage = $faker->numberBetween(1,3);
+
+            for ($j=0;$j<$nbFormationStage;$j++)
+            {
+                $stage->addFormation($tabFormations[$faker->numberBetween(0,count($tabFormations)-1)]);
+            }
+
+            $manager->persist($stage);
         }
 
         $manager->flush();
