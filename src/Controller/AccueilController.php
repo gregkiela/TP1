f9298c36219d7b187a7e07ca33572ad161142f46
Gregory Errecart <gerrecart@iutbayonne.univ-pau.fr>
2022-01-08 14:57:35 +0100

    Exriture du code du controller Accueil pour récupérer les données en BD

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Entreprise;
+use App\Entity\Formation;
+use App\Entity\Stage;
+
 class AccueilController extends AbstractController
 {
     /**
@@ -13,8 +17,22 @@ class AccueilController extends AbstractController
      */
     public function index(): Response
     {
+        //Récupérer le repository de nos entités Entreprise, Formation et Stage
+        $entrepriseRepository = $this->getDoctrine()->getRepository(Entreprise::class);
+        $formationRepository = $this->getDoctrine()->getRepository(Formation::class);
+        $stageRepository = $this->getDoctrine()->getRepository(Stage::class);
+
+        //Récupérer les donneées stockés dans notre base;
+        $donneesEntreprises = $entrepriseRepository->findAll();
+        $donneesFormations = $formationRepository->findAll();
+        $donneesStages = $stageRepository->findAll();
+
+        //Envoyer les données récupérées
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'entreprises => $donneesEntreprises',
+            'formations => $donneesFormations',
+            'stages => $donneesStages',
         ]);
     }
 }
