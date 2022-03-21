71f74ebf5eb201c84f273f4c69f6b76395b58db1
Gregory Errecart <gerrecart@iutbayonne.univ-pau.fr>
2022-03-21 16:19:20 +0100

    Ajout de bouton proposant la connexion ou la déconnexion en fonction de l'état de connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,8 +29,8 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout(): void
+    public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        
     }
 }
