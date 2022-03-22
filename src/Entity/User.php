3f39b3913080d60bbe2ffef0159704f65bc61b03
Gregory Errecart <gerrecart@iutbayonne.univ-pau.fr>
2022-03-22 10:13:39 +0100

    Sécurisation de la saisie du formulaire d'inscription

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,7 +22,7 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      * @Assert\Email(
-     *  message = "L'email '{{ value }} ' n'est pas valide")
+     *  message = "L'email n'est pas valide")
      */
     private $email;
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -15,7 +18,14 @@ class UserType extends AbstractType
             ->add('prenom')
             ->add('nom')
             ->add('email')
-            ->add('password')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe saisies ne correspondent pas',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répétez la saisie du mot de passe'],
+            ])
         ;
     }
 
